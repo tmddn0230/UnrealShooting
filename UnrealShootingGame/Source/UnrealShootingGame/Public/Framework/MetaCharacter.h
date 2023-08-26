db21c15929b8098fcb879f3087fd7f9083e28cc7
YU <82423176+tmddn0230@users.noreply.github.com>
2023-08-26 13:44:49 +0900

    Character VR Hand
    VR Hand 붙이는 과정에서 알게 된 것 Character.txt
    UI 간단한 작업 및 전용 캐릭터 생성

--- UnrealShootingGame/Source/UnrealShootingGame/Public/Framework/MetaCharacter.h
+++ UnrealShootingGame/Source/UnrealShootingGame/Public/Framework/MetaCharacter.h
@@ -4,6 +4,10 @@
 
 #include "CoreMinimal.h"
 #include "GameFramework/Character.h"
+
+#include "MotionControllerComponent.h"
+#include "Components/SkeletalMeshComponent.h"
+
 #include "MetaCharacter.generated.h"
 
 UCLASS()
@@ -19,11 +23,18 @@ protected:
 	// Called when the game starts or when spawned
 	virtual void BeginPlay() override;
 
+
 public:	
 	// Called every frame
 	virtual void Tick(float DeltaTime) override;
 
 	// Called to bind functionality to input
 	virtual void SetupPlayerInputComponent(class UInputComponent* PlayerInputComponent) override;
+
+	// MotionController 
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR")
+	//UMotionControllerComponent* MotionControllerLeft;
+	//UPROPERTY(EditAnywhere, BlueprintReadWrite, Category = "VR")
+	//UMotionControllerComponent* MotionControllerRight;
 
 };
